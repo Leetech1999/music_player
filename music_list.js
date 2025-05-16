d38f2ba947997d639fc6952d1c9dae5e5200b72c
Leetech1999 <dlwjdwns999@naver.com>
2025-05-16 14:58:37 +0900

    Initial commit

---
+++ music_list.js
@@ -0,0 +1,8 @@
+const musicList = [
+    { name:"Alone", artist:"Color Out", audio:"Alone.mp3",img:"album1", total:"04:05" },
+    { name:"Mahidevran - Maze of sorrow", artist:"Mahidevran rock band", audio:"Mahidevran-Maze_of_sorrow.mp3",img:"album2", total:"06:28" },
+    { name:"No Rest Or Endless Rest", artist:"Lisofv", audio:"No_Rest_Or_Endless_Rest.mp3",img:"album3", total:"04:33" },
+    { name:"Old Melody", artist:"TAB", audio:"Old_Money.mp3",img:"album4", total:"04:05" },
+    { name:"Tantalizing Youth", artist:"Social Square", audio:"Tantalizing_Youth.mp3",img:"album5", total:"04:12" },
+    { name:"The Deep", artist:"Anitek", audio:"The_Deep.mp3",img:"album6", total:"02:24" }
+]
